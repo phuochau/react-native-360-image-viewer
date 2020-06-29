723a3a126fa4b7232bed3d60c2ee09a265a2df31
Hau Vo <hau.vophuoc@gmail.com>
2020-06-30 00:26:49 +0700

    Done first version

---
+++ README.md
@@ -0,0 +1,36 @@
+react-native-360-image-viewer
+===
+
+Inspired by [https://github.com/scaleflex/js-cloudimage-360-view](https://github.com/scaleflex/js-cloudimage-360-view)
+
+![Sample](http://g.recordit.co/Ek5o1aI2Jo.gif)
+
+Images credit: [https://www.cloudimage.io/](https://www.cloudimage.io/)
+
+## Installation
+
+```bash
+yarn add react-native-360-image-viewer
+
+or
+
+npm install react-native-360-image-viewer --save
+```
+
+Please file an issue if you have any trouble!
+
+
+## Usage
+
+See [https://github.com/phuochau/react-native-360-image-viewer/tree/master/example](https://github.com/phuochau/react-native-360-image-viewer/tree/master/example)
+
+## Properties
+option | Info
+------ | ----
+srcset | Array of images. The image index should follow the ascending rotation. If you provide more images, the animation will be smoother. 
+width | Width of image 
+height | height of image 
+
+## License
+
+MIT
---
+++ example/App.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import _ from 'lodash'
+import {
+  View,
+  Dimensions
+} from 'react-native';
+
+import Image360Viewer from 'react-native-360-image-viewer'
+
+const { width, height } = Dimensions.get('window')
+const images = _.reverse([
+  require(`./images/iris-1.jpg`),
+  require(`./images/iris-2.jpg`),
+  require(`./images/iris-3.jpg`),
+  require(`./images/iris-4.jpg`),
+  require(`./images/iris-5.jpg`),
+  require(`./images/iris-6.jpg`),
+  require(`./images/iris-7.jpg`),
+  require(`./images/iris-8.jpg`),
+  require(`./images/iris-9.jpg`),
+  require(`./images/iris-10.jpg`),
+  require(`./images/iris-11.jpg`),
+  require(`./images/iris-12.jpg`),
+  require(`./images/iris-13.jpg`),
+  require(`./images/iris-14.jpg`),
+  require(`./images/iris-15.jpg`),
+  require(`./images/iris-16.jpg`),
+  require(`./images/iris-17.jpg`),
+  require(`./images/iris-18.jpg`),
+  require(`./images/iris-19.jpg`),
+  require(`./images/iris-20.jpg`),
+  require(`./images/iris-21.jpg`),
+  require(`./images/iris-22.jpg`),
+  require(`./images/iris-23.jpg`),
+  require(`./images/iris-24.jpg`),
+  require(`./images/iris-25.jpg`),
+  require(`./images/iris-26.jpg`),
+  require(`./images/iris-27.jpg`),
+  require(`./images/iris-28.jpg`),
+  require(`./images/iris-29.jpg`),
+  require(`./images/iris-30.jpg`),
+  require(`./images/iris-31.jpg`),
+  require(`./images/iris-32.jpg`),
+  require(`./images/iris-33.jpg`),
+  require(`./images/iris-34.jpg`),
+  require(`./images/iris-35.jpg`),
+  require(`./images/iris-36.jpg`)
+])
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Image360Viewer srcset={images} width={width} height={height} />
+    </View>
+  );
+};
+
+export default App;
